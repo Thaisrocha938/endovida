e91c8390ce357c134275970f83211d3fca1f989a
Thais Rocha Costa <thatath938@gmail.com>
2021-11-18 21:00:55 -0300

    Initial commit

---
+++ src/main/java/com/jk/endovida/EndovidaApplication.java
@@ -0,0 +1,13 @@
+package com.jk.endovida;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EndovidaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EndovidaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jk/endovida/EndovidaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jk.endovida;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EndovidaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
